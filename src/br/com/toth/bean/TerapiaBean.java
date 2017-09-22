ce8f639fe994626dfe6b5deb667fd3813ff1c14d
ChristopherNSMM <christopher.nicolas.mauricio@gmail.com>
2017-09-22 15:25:26 -0300

    Trabalho salvo para o fds, inicio nada configurado.

---
+++ src/br/com/toth/bean/TerapiaBean.java
@@ -0,0 +1,19 @@
+package br.com.toth.bean;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TerapiaBean {
+	
+	
+//    private Livro livro = new Livro();
+
+//    public Livro getLivro() {
+//        return livro;
+//    }
+	
+	
+    public void gravar() {
+        System.out.println("Gravando... ");
+    }
+
+}
